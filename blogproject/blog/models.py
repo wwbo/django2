dae70a60631faeb09fc4f7aedee51b941d54ccc0
nanfengpo <lsxxxxxx@126.com>
2017-09-12 21:27:28 +0800

    Initial commit

---
+++ blogproject/blog/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    excerpt = models.CharField(max_length=200, blank=True)
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+    author = models.ForeignKey(User)
+
+    views=models.PositiveIntegerField(default=0)
+    def increase_views(self):
+        self.views +=1
+        self.save(update_fields=['views'])
+
+
+
+
+
+    def get_absolute_url(self):
+        return reverse('blog:detail',kwargs={'pk':self.pk})
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering=['-created_time','-modified_time']
+
